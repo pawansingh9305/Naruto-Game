1f2fb4fcad85f8a4de92216097fcd649b1068059
Pawan Singh <106629214+pawansingh9305@users.noreply.github.com>
2024-01-08 22:39:57 +0530

    Update script.js

--- game/js/script.js
+++ game/js/script.js
@@ -8,7 +8,8 @@ audiogo=new Audio('../music/akatsuki.mp3');
 
 setTimeout(()=>{
 audio.play();
-},500);
+},100);
+
 document.onkeydown = function(e){
     console.log("key code is:",e.keyCode);
     if (e.keyCode==38){
